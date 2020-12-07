cddd36eb616d44b94e6ecd0520de0d97679c96c3
Radek <283680@pw.edu.pl>
2020-12-07 10:02:29 +0100

    basic activity for image capturing

--- app/src/main/java/com/example/photofingerend/MainActivity.java
+++ app/src/main/java/com/example/photofingerend/MainActivity.java
@@ -1,14 +1,132 @@
 package com.example.photofingerend;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private CircleImageView fingerprintImageView;
+
+    public String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fingerprintImageView = findViewById(R.id.fingerprintImage);
+        fingerprintImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    // TODO: obsluga gdy nie da sie utworzyc pliku do zdjecia
+                    e.printStackTrace();
+                }
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            "com.example.photofingerend.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        View identifyButton = findViewById(R.id.identifyButton);
+        identifyButton.setVisibility(View.VISIBLE);
+
+        TextView instructionText = findViewById(R.id.instructionText);
+        instructionText.setVisibility(View.INVISIBLE);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            setPic();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.GERMANY).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = fingerprintImageView.getWidth();
+        int targetH = fingerprintImageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        fingerprintImageView.setImageBitmap(bitmap);
+    }
+
+    public void processPhoto(View view) {
+        // pokaz krecace kulko
+
+        // klasa co ma metody do przetwarzania tego obrazu
+        // i z tej klasy jest zwrocone imie osoby asynchronicznie (w oddzielnym watku)
+        // maja byc tez zwrocone obrazy poszczegolnych etapow (przypisac do odpowiednich pol statycznych ShowActivity)
+        // jak juz zwroci to ukryj krecace kulko
+
+        // pokaz instruction text i napisz czyj to palec w nim
+
+        // pokaz przycisk "pokaz etapy"
+    }
+
+    public void showSteps(View view) {
+        // uruchom nowe activity i tam wyswietlaj te etapy ktore zostaly juz zainicjalizowane
     }
 }
---
+++ app/src/main/java/com/example/photofingerend/ProcessImage.java
@@ -0,0 +1,17 @@
+package com.example.photofingerend;
+
+import android.graphics.Bitmap;
+
+import java.util.concurrent.Executor;
+
+public class ProcessImage {
+    private final Executor executor;
+
+    public ProcessImage(Executor executor) {
+        this.executor = executor;
+    }
+
+    public void processImage(Bitmap bmp) {
+
+    }
+}
