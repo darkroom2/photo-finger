cc7deb671b06945bc2555f3faf66bb952c994627
Radek <283680@pw.edu.pl>
2020-12-10 21:29:02 +0100

    some processing done, matching.

--- app/src/main/java/com/example/photofingerend/MainActivity.java
+++ app/src/main/java/com/example/photofingerend/MainActivity.java
@@ -1,28 +1,39 @@
 package com.example.photofingerend;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.machinezoo.sourceafis.FingerprintImage;
+import com.machinezoo.sourceafis.FingerprintMatcher;
+import com.machinezoo.sourceafis.FingerprintTemplate;
+
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -84,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             File photoFile = null;
             try {
-                photoFile = createImageFile();
+                photoFile = createImageFile(".jpg");
             } catch (IOException e) {
                 // TODO: obsluga gdy nie da sie utworzyc pliku do zdjecia
                 // TODO: usuwanie zdjec po identyfikacji
@@ -116,13 +127,13 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private File createImageFile() throws IOException {
+    private File createImageFile(String ext) throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.GERMANY).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,
-                ".jpg",
+                ext,
                 storageDir
         );
 
@@ -155,16 +166,75 @@ public class MainActivity extends AppCompatActivity {
         fingerprintImageView.setImageBitmap(bitmap);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void processPhoto(View view) {
         resultText.setVisibility(View.INVISIBLE);
         instructionText.setText(R.string.wait_tip);
 
+        testowa();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void testowa() {
         executorService.execute(() -> {
 
+            String result = "";
+
             if (openCvLoaded) {
-                ProcessImage pi = new ProcessImage("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/Inz_Rado_Lewa_Maly3.jpg");
+                ProcessImage pi = new ProcessImage("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/lw.png");
 //                ProcessImage pi = new ProcessImage(currentPhotoPath);
-                pi.processImage();
+
+                File temp = null;
+                try {
+                    temp = createImageFile(".png");
+                } catch (IOException e) {
+                    // TODO: obsluga gdy nie da sie utworzyc pliku do zdjecia
+                    // TODO: usuwanie zdjec po identyfikacji
+                    e.printStackTrace();
+                }
+
+                byte[] probeImage = new byte[0];
+                try {
+                    FileOutputStream fos = new FileOutputStream(temp);
+                    pi.getEnchanced().compress(Bitmap.CompressFormat.PNG, 100, fos);
+                    probeImage = Files.readAllBytes(temp.toPath());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                ////            try {
+////                FileOutputStream fos = new FileOutputStream(skeletonImg);
+////                pi.getEnchanced().compress(Bitmap.CompressFormat.PNG, 100, fos);
+////            } catch (IOException e) {
+////                e.printStackTrace();
+////            }
+
+                FingerprintTemplate probe = new FingerprintTemplate(
+                        new FingerprintImage()
+                                .dpi(500)
+                                .decode(probeImage));
+
+                File dirTemplates = new File("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/Wzorce");
+
+                List<UserDetails> users = new ArrayList<>();
+
+                for (File file : dirTemplates.listFiles()) {
+                    byte[] serialized = new byte[0];
+                    try {
+                        serialized = Files.readAllBytes(file.toPath());
+                        FingerprintTemplate template = new FingerprintTemplate(serialized);
+                        users.add(new UserDetails(new Random().nextInt(150), file.getName(), template));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                UserDetails found = find(probe, users);
+
+                if(found != null)
+                    result = found.name;
+
             }
 
             // otworz tutaj zdjecie i wyslij do klasy przetwarzajacej obraz
@@ -172,12 +242,12 @@ public class MainActivity extends AppCompatActivity {
             // klasa co ma metody do przetwarzania tego obrazu
             // i z tej klasy jest zwrocone imie osoby
             // maja byc tez zwrocone obrazy poszczegolnych etapow (przypisac do odpowiednich pol statycznych ShowActivity)
-            String result = "Radek";
 
             // po skonczeniu identyfikacji odswiezamy watek UI i konczymy prace (powracajac do watku UI)
+            String finalResult = result;
             runOnUiThread(() -> {
                 // pokaz instruction text i napisz czyj to palec
-                resultText.setText(String.format(getResources().getString(R.string.result_person), result));
+                resultText.setText(String.format(getResources().getString(R.string.result_person), finalResult));
                 resultText.setVisibility(View.VISIBLE);
 
                 instructionText.setText(R.string.show_steps_tip);
@@ -187,7 +257,89 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private void dodajBaze() {
+//        File dirImgs = new File("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/Baza/Obrazy");
+//        File dirSkeletons = new File("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/Baza/Szkielety");
+//        File dirTemplates = new File("/storage/emulated/0/Android/data/com.example.photofingerend/files/Pictures/Baza/Wzorce");
+//
+//        for (File file : dirSkeletons.listFiles()) {
+////        for (File file : dirImgs.listFiles()) {
+////            ProcessImage pi = new ProcessImage(file.toPath().toString());
+////
+////            int i = file.getName().lastIndexOf('.');
+////            String name = file.getName().substring(0, i);
+////
+////            File skeletonImg = new File(dirSkeletons, name + ".png");
+////
+////            try {
+////                FileOutputStream fos = new FileOutputStream(skeletonImg);
+////                pi.getEnchanced().compress(Bitmap.CompressFormat.PNG, 100, fos);
+////            } catch (IOException e) {
+////                e.printStackTrace();
+////            }
+////
+//            byte[] image = new byte[0];
+//            try {
+//                image = Files.readAllBytes(file.toPath());
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            FingerprintTemplate template = new FingerprintTemplate(
+//                    new FingerprintImage()
+//                            .dpi(500)
+//                            .decode(image));
+//            byte[] serialized = template.toByteArray();
+//
+//            int i = file.getName().lastIndexOf('.');
+//            String name = file.getName().substring(0, i);
+//            File templateFile = new File(dirTemplates, name + ".json.gz");
+//            try {
+//                FileOutputStream fos = new FileOutputStream(templateFile);
+//                fos.write(serialized);
+//                fos.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        System.out.println("brk pt");
+////        byte[] image = Files.readAllBytes(Paths.get("fingerprint.jpeg"));
+////        FingerprintTemplate template = new FingerprintTemplate(
+////                new FingerprintImage()
+////                        .dpi(500)
+////                        .decode(image));
+////        byte[] serialized = template.toByteArray();
+//    }
+
     public void showSteps(View view) {
         // uruchom nowe activity i tam wyswietlaj te zdj etapow ktore zostaly juz zainicjalizowane
     }
+
+    static class UserDetails {
+        int id;
+        String name;
+        FingerprintTemplate template;
+
+        public UserDetails(int id, String name, FingerprintTemplate template) {
+            this.id = id;
+            this.name = name;
+            this.template = template;
+        }
+    }
+
+    UserDetails find(FingerprintTemplate probe, Iterable<UserDetails> candidates) {
+        FingerprintMatcher matcher = new FingerprintMatcher().index(probe);
+        UserDetails match = null;
+        double high = 0;
+        for (UserDetails candidate : candidates) {
+            double score = matcher.match(candidate.template);
+            if (score > high) {
+                high = score;
+                match = candidate;
+            }
+        }
+        double threshold = 30;
+        return high >= threshold ? match : null;
+    }
+
 }
--- app/src/main/java/com/example/photofingerend/ProcessImage.java
+++ app/src/main/java/com/example/photofingerend/ProcessImage.java
@@ -4,15 +4,20 @@ package com.example.photofingerend;
 import android.graphics.Bitmap;
 import android.util.Log;
 
+import com.machinezoo.sourceafis.FingerprintMatcher;
+import com.machinezoo.sourceafis.FingerprintTemplate;
+
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
+import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.core.CvException;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.CLAHE;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
@@ -20,15 +25,20 @@ import java.util.List;
 
 public class ProcessImage {
 
-    private final Mat original;
+    private final Bitmap original;
+    private final Bitmap enhanced;
 
     public ProcessImage(String photoPath) {
-        original = Imgcodecs.imread(photoPath); // BGR order
+        Mat original = Imgcodecs.imread(photoPath); // BGR order
+        this.original = matToBmp(original);
+
+        Mat enhanced = processImage(original);
+        this.enhanced = grayMatToBmp(enhanced);
     }
 
-    public void processImage() {
+    public Mat processImage(Mat input) {
         /// Original input
-        Mat src = original.clone();
+        Mat src = input.clone();
         Bitmap bSrc = matToBmp(src);
         System.out.println("wyswietl oryginal brejkpojnt");
 
@@ -55,35 +65,87 @@ public class ProcessImage {
         Core.bitwise_and(gray, gray, grayMasked, skinMask);
         Bitmap bGrayMasked = grayMatToBmp(grayMasked);
         System.out.println("wyswietl bGrayMasked brejkpojnt");
-//
-//        """Equalize histogram"""
-//        clahe = cv.createCLAHE(clipLimit=32.0, tileGridSize=(100, 100))
-//        equalized = clahe.apply(gray)
-//    # equalized = cv.equalizeHist(gray)
-//    # cv.imshow('equalized', equalized)
-//    # cv.waitKey()
-//
-//        block_size = 16
-//
-//        """Find ridges mask"""
-//        mask2 = findRidges(resized, block_size, 0.35)
-//    # cv.imshow('mask', mask * 255)
-//    # cv.waitKey()
-//
-//        """Blur the equalized to remove noise and preserve edges"""
-//    # kernel = np.ones((5, 5), np.float32) / 25
-//    # blurred = cv.filter2D(equalized, -1, kernel)
-//        blurred = cv.bilateralFilter(equalized, 5, 200, 200)
-//    # blurred = cv.GaussianBlur(equalized, (5, 5), 2)
-//    # cv.imshow('blurred', blurred)
-//    # cv.waitKey()
-//
-//        """Threshold the image with adaptive thresholding"""
-//        threshed = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 1)
-//    # threshed = cv.threshold(blurred, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
-//
-//        return 255 - threshed
 
+        /// Equalize histogram
+        CLAHE clahe = Imgproc.createCLAHE(32.0, new Size(100, 100));
+        Mat equalized = new Mat();
+        clahe.apply(grayMasked, equalized);
+        Bitmap bEqualized = grayMatToBmp(equalized);
+        System.out.println("wyswietl bEqualized brejkpojnt");
+
+        /// Block size for processing
+        int blockSize = 16;
+
+        /// Get ridges mask
+        Mat ridgesMask = findRidges(equalized, skinMask, blockSize, 0.35);
+        Bitmap bRidgesMask = grayMatToBmp(ridgesMask);
+        System.out.println("wyswietl bRidgesMask brejkpojnt");
+
+        /// Blur the equalized to remove noise and preserve edges
+        Mat filtered = new Mat();
+        Imgproc.bilateralFilter(equalized, filtered, 5, 200, 200);
+        Bitmap bFiltered = grayMatToBmp(filtered);
+        System.out.println("wyswietl bFiltered brejkpojnt");
+
+        /// Threshold the image with adaptive thresholding
+        Mat threshed = new Mat();
+        Imgproc.adaptiveThreshold(filtered, threshed, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 1);
+
+        /// Invert image so ridges are black.
+        Core.bitwise_not(threshed, threshed);
+        Bitmap bThreshed = grayMatToBmp(threshed);
+        System.out.println("wyswietl bThreshed brejkpojnt");
+
+        /// Merge two masks to not include something we do not want to process
+        Mat mask = new Mat();
+        Core.bitwise_and(skinMask, ridgesMask, mask);
+        Bitmap bMask = grayMatToBmp(mask);
+        System.out.println("wyswietl bMask brejkpojnt");
+
+        /// Apply mask to threshed image
+        Core.bitwise_and(threshed, mask, threshed);
+        bThreshed = grayMatToBmp(threshed);
+        System.out.println("wyswietl bThreshed2 brejkpojnt");
+
+        return threshed;
+    }
+
+    private Mat findRidges(Mat src, Mat mask, int blockSize, double threshold) {
+        Size srcSize = src.size();
+        Mat varianceImg = new Mat(srcSize, CvType.CV_32F);
+        Mat ridgeMask = new Mat(srcSize, CvType.CV_8U);
+
+        MatOfDouble std = new MatOfDouble();
+
+        Core.meanStdDev(src, new MatOfDouble(), std, mask);
+
+        threshold = std.toArray()[0] * threshold;
+
+        for (int x = 0; x < srcSize.width - blockSize; x += blockSize) {
+            for (int y = 0; y < srcSize.height - blockSize; y += blockSize) {
+                // TODO: rozwiazac problem wyjscia poza tablice
+                Core.meanStdDev(src.submat(y, y + blockSize, x, x + blockSize), new MatOfDouble(), std, mask.submat(y, y + blockSize, x, x + blockSize));
+                double variance = std.toArray()[0];
+                varianceImg.submat(y, y + blockSize, x, x + blockSize).setTo(Scalar.all(variance));
+            }
+        }
+
+        Core.compare(varianceImg, new Scalar(threshold), ridgeMask, Core.CMP_GT);
+
+        Imgproc.morphologyEx(ridgeMask, ridgeMask, Imgproc.MORPH_CLOSE, Mat.ones(new Size(17, 17), CvType.CV_8U));
+        Imgproc.erode(ridgeMask, ridgeMask, Mat.ones(new Size(51, 51), CvType.CV_8U));
+
+        return ridgeMask;
+    }
+
+    private void testowaWypiszMat(Mat varianceImg) {
+        String ehh = "";
+        for (int i = 200; i < varianceImg.height() - 200; ++i) {
+            for (int j = 0; j < varianceImg.width(); ++j) {
+                ehh += Double.toString(varianceImg.get(i, j)[0]);
+            }
+            ehh += '\n';
+        }
     }
 
     private Mat getSkinMask(Mat src) {
@@ -102,7 +164,7 @@ public class ProcessImage {
         Core.inRange(imgHsv, lowerBounds, upperBounds, mask);
 
         // jadro operacji morfologicznej otwierania i zamykania
-        Mat kernel = new Mat(new Size(9, 9), CvType.CV_8UC1, Scalar.all(1));
+        Mat kernel = new Mat(new Size(9, 9), CvType.CV_8U, Scalar.all(1));
 
         // Otwarcie usuwa małe obiekty z pierwszego planu (zwykle brane jako jasne piksele) obrazu, umieszczając je w tle
         Imgproc.morphologyEx(mask, mask, Imgproc.MORPH_OPEN, kernel);
@@ -131,12 +193,8 @@ public class ProcessImage {
         cnt.add(contours.get(maxAreaIdx));
 
         // wypelnienie najwiekszego konturu na bialo (na 1) TODO: zmienic na 1 z 255
-        Mat skinMask = new Mat(_src.width(), _src.height(), CvType.CV_8UC1, Scalar.all(0));
-        Imgproc.fillPoly(skinMask, cnt, Scalar.all(1));
-
-//        Mat end = new Mat();
-//        Core.bitwise_and(_src, _src, end, skinMask);
-//        return end;
+        Mat skinMask = new Mat(_src.width(), _src.height(), CvType.CV_8U, Scalar.all(0));
+        Imgproc.fillPoly(skinMask, cnt, Scalar.all(255));
 
         return skinMask;
     }
@@ -147,6 +205,22 @@ public class ProcessImage {
         try {
             bmp = Bitmap.createBitmap(_src.cols(), _src.rows(), Bitmap.Config.ARGB_8888);
             Imgproc.cvtColor(_src, _src, Imgproc.COLOR_BGR2RGB);
+            Utils.matToBitmap(_src, bmp);
+            return bmp;
+        } catch (CvException e) {
+            Log.d("Exception", e.getMessage());
+            return null;
+        }
+    }
+
+    private Bitmap floatMatToBmp(Mat src) {
+        Mat _src = src.clone();
+        Bitmap bmp;
+        try {
+            bmp = Bitmap.createBitmap(_src.cols(), _src.rows(), Bitmap.Config.ARGB_8888);
+            _src.convertTo(_src, CvType.CV_8U);
+            Core.normalize(_src, _src, 0, 255, Core.NORM_MINMAX);
+            Imgproc.cvtColor(_src, _src, Imgproc.COLOR_GRAY2RGB);
             Utils.matToBitmap(_src, bmp);
             return bmp;
         } catch (CvException e) {
@@ -168,4 +242,14 @@ public class ProcessImage {
             return null;
         }
     }
+
+
+    public Bitmap getOriginal() {
+        return original;
+    }
+
+    public Bitmap getEnchanced() {
+        return enhanced;
+    }
+
 }
